1b9b83ab7899c4a1d324febb4f25839c791902c8
maurolepore <maurolepore@gmail.com>
2018-02-12 11:46:21 -0500

    Bild site.

--- R/replace_all_na.R
+++ R/replace_all_na.R
@@ -1,17 +1,17 @@
 #' Replace all NA's of a dataframe with a filler
 #'
-#' @param df A dataframe.
-#' @param replace A filler; whatever you want to replace NA's with.
+#' @param x A dataframe.
+#' @param filler A filler; whatever you want to replace NA's with.
 #'
-#' @return A modified versio of `df`.
+#' @return A modified versio of `x`.
 #' @export
 #'
 #' @examples
-#' df <- data.frame(x = c(NA, 1), y = c("a", NA), stringsAsFactors = FALSE)
-#' replace_all_na(df)
-#' replace_all_na(df, "")
-#' replace_all_na(df, "missing")
-replace_all_na <- function(df, filler = 0) {
-  stopifnot(is.data.frame(df))
-  replace(df, is.na(df), filler)
+#' x <- data.frame(x = c(NA, 1), y = c("a", NA), stringsAsFactors = FALSE)
+#' replace_all_na(x)
+#' replace_all_na(x, "")
+#' replace_all_na(x, "missing")
+replace_all_na <- function(x, filler = 0) {
+  stopifnot(is.data.frame(x))
+  replace(x, is.na(x), filler)
 }
