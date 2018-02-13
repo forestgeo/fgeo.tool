07d93c61a4a8f967b20cde753026bc9e7ff774de
maurolepore <maurolepore@gmail.com>
2018-02-13 12:09:59 -0500

    Acknowledge help with fill_na() (closes #22).

--- R/replace_all_na.R
+++ R/replace_all_na.R
@@ -1,4 +1,7 @@
-#' Replace all NA's of a dataframe with a filler
+#' Fill NA's with a single filler.
+#' 
+#' This is a wrapper of [base::replace()] with a simplified interface and a name
+#' that target the specific job of filling NA's.
 #'
 #' @param x A dataframe.
 #' @param filler A filler; whatever you want to replace NA's with.
@@ -6,15 +9,29 @@
 #' @return A modified versio of `x`.
 #' 
 #' @section Acknowledgements:
-#'   This function was inspired by David Kenfack.
+#'   This function was inspired by conversations with David Kenfack and by a 
+#'   [blog post by Rick Pack](https://goo.gl/aMTQck). Also tanks to Edgar Ruiz
+#'   (@edgararuiz) for discussing alternatives 
+#'   (via [this blog post](https://goo.gl/4uME2U) on RStudio community).
 #' @export
 #'
 #' @examples
 #' x <- data.frame(x = c(NA, 1), y = c("a", NA), stringsAsFactors = FALSE)
-#' replace_all_na(x)
-#' replace_all_na(x, "")
-#' replace_all_na(x, "missing")
-replace_all_na <- function(x, filler = 0) {
-  stopifnot(is.data.frame(x))
-  replace(x, is.na(x), filler)
+#' 
+#' a_dataframe <- x
+#' fill_na(a_dataframe)
+#' fill_na(a_dataframe, "")
+#' fill_na(a_dataframe, "missing")
+#' 
+#' a_matrix <- as.matrix(x)
+#' fill_na(a_matrix)
+#' 
+#' a_vector <- x$x
+#' fill_na(a_vector)
+#' 
+#' a_list <- list(x, x, x)
+#' lapply(a_list, fill_na)
+fill_na <- function(x, filler = 0) {
+  x[is.na(x)] <- filler
+  x
 }
--- tests/testthat/test-replace_all_na.R
+++ tests/testthat/test-replace_all_na.R
@@ -1,13 +1,13 @@
-context("replace_all_na")
+context("fill_na")
 
 df <- tibble::tibble(x = c(NA, 1), y = c("a", NA))
 
 test_that("errs with wrong input", {
-  expect_error(replace_all_na(1))
+  expect_error(fill_na(1))
 })
 
 test_that("returns expected value", {
-  out <- replace_all_na(df, "z")
+  out <- fill_na(df, "z")
   expect_equal(out$x[[1]], "z")
   expect_equal(out$y[[2]], "z")
 })
