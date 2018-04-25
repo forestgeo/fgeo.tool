188907f6f5778a2ae7f22b26b3cc968758fbd684
maurolepore <maurolepore@gmail.com>
2018-04-25 10:18:36 -0400

    Fix build error: Avoid depencency on ggplot2.

--- R/utils.R
+++ R/utils.R
@@ -24,22 +24,3 @@ each_list_item_is_df <- function(x) {
   }
   all(purrr::map_lgl(x, has_class_df))
 }
-
-
-
-# plyr --------------------------------------------------------------------
-
-#' Round to multiple of any number.
-#' 
-#' Copied from `plyr:::round_any.numeric()`.
-#' 
-#' @param x Numeric vector to round.
-#' @param accuracy Number to round to.
-#' @param f Rounding function: floor, ceiling or round.
-#' 
-#' @seealso `plyr::round_any()` and \url{http://bit.ly/2JrBQK3}.
-#' 
-#' @noRd
-round_any <- function(x, accuracy, f = round) {
-  f(x / accuracy) * accuracy
-}
