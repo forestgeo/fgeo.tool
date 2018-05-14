3340502df843ea23dfd4688e8c3b26b613580962
maurolepore <maurolepore@gmail.com>
2018-05-14 11:56:49 -0400

    Update doc

--- R/lookup.R
+++ R/lookup.R
@@ -8,6 +8,9 @@
 #' @param old,new Vectors of equal length giving old and new codes.
 #'
 #' @seealso `dplyr::recode()`
+#' 
+#' @section Acknowledgment:
+#' Thanks to David Kenfack for inspiring this function.
 #'
 #' @return A "spliced" list with names from `old` and values from `new`. The
 #' kind of data structure that you can feed to `...` in dplyr::recode()`.
@@ -24,16 +27,14 @@
 #'   new = c("spp3", "spp4")
 #' )
 #'
-#' lookup(look$old, look$new)
+#' sp <- c("spp1", "spp2", "spp3", "unknown", "spp3", "unknown", "spp1", "spp1")
+#' recode(sp, lookup(look$old, look$new))
 #'
-#' x <- c("spp1", "spp2", "spp3", "unknown", "spp3", "unknown", "spp1", "spp1")
-#' x
-#' recode(x, lookup(look$old, look$new))
-#' # Same
-#' recode(x, !!!as.list(set_names(look$new, look$old)))
-#'
-#' dfm <- tibble(x = x)
-#' mutate(dfm, new_x = recode(x, lookup(look$old, look$new)))
+#' census <- tibble(sp = sp)
+#' mutate(census, new_sp = recode(sp, lookup(look$old, look$new)))
+#' 
+#' # Overwrite
+#' mutate(census, sp = recode(sp, lookup(look$old, look$new)))
 lookup <- function(old, new) {
   if (length(old) != length(new)) {
     stop("`old` and `new` must be of equal length.")
