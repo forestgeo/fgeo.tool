81fbc38830440cb0322f117dda216428764cfc9a
maurolepore <maurolepore@gmail.com>
2018-05-30 22:51:40 -0400

    Organize funs.

--- tmp.R
+++ tmp.R
@@ -1,9 +1,12 @@
+
 # SPECIFIC TO FGEO --------------------------------------------------------
 
-# Add columns to a dataframe
+# fgeo.tool: SPECIFIC TO FGEO, DEPENDENT ----------------------------------
+
+# add_*(): Add columns to a dataframe
 # georeference: position stems in a forest plot
 # TODO: Maybe change to add_plot_*()
-add_var  # TODO: Change title to reflect it belongs to thig group of functions
+add_var  # TODO: Don't export
 add_col_row
 add_col_row2
 add_hectindex
@@ -12,41 +15,40 @@ add_lxly
 add_quad
 add_qxqy
 add_subquad
-# xxx what's missing here?
-# add_censusid?
-# add_plotid?
-# add_plotid?
 
 # Add columns to a dataframe
-# Determine status
+# stts_*(): Determine status
 add_status_tree
-# xxx what's missing here?
-# TODO: Transform Condit's algorithm into a function that determines status
-#       it may be a case_when
-to_recensus
 
+
+# xxx cont here
+
+
+# Maybe remove or not export
 # filter status
 row_discard_twice_dead
 row_filter_status
 row_keep_alive_stem
 row_keep_alive_tree
 
-# filter other
-row_*
-row_top
-
 # Sanitize
 recode_subquad
+# Maybe pull_elev or use_elev
 restructure_elev
 
+
 # read fgeo data
+# TODO: Add more compleate read_*() functions.
+# TODO: Maybe rename to col_types_*() or types_*()
 type_fgeo
 type_taxa
 type_vft
-# TODO: Add more compleate read_*() functions.
+
 
 # Create stuff
 create_habitat
+
+# Maybe put on hold, don't export right now?
 # Fieldforms
 fieldforms_header
 fieldforms_output
@@ -56,39 +58,61 @@ fieldforms_prepare
 
 # GENERAL -- NOT SPECIFIC TO FGEO -----------------------------------------
 
-# dev
+# filter other
+row_*
+row_top
+
+
+
+# fgeo.dev or fgeo.base
 # Output condition: Called for side effects: Trow messages, warnings
+# Maybe make it s3 (check_unique.numeric, check_unique.data.frame, etc.)
+# No external dependencies. Maybe move to fgeo.base
 check_unique
 check_unique_vector
 # output logical
 exists_in_pkg
 
+
+# fgeo.dev or fgeo.base
 # help work with fgeo packages, functions, datasets.
 example_path
 # search objects by names
-find_data_of_class
+# TODO: Make it base only
 hide_data_of_class
+show_data_of_class
 
+
+
+
+
+# Maybe remove
+
+# Maybe use modify_if() instead and/or don't export
 type_ensure
 
+# Do I need this? Where do I use it?
 # summarize
 count_duplicated
 
 # Sanitize
+# Move to fgeo.base?
 fill_na
+
 lookup
 replace_null
 
 # Work on names
+# replace with fgeo.base::name_df_lst()
 ls_name_df
-nms_*
+
 
 
 # Read/write; inputs and outputs; to/from file/data
 csv_to_df
 csv_to_df_lst
 files_to_df
-ls_csv_to_df
+ls_csv_df
 ls_join_df
 ls_list_spreadsheets
 xl_sheets_to_csv
