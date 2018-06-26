fb3726360691ec50feee4425472b0a41f16c27b4
maurolepore <maurolepore@gmail.com>
2018-06-26 16:18:55 -0400

    Reexport tibble and friends.

--- R/imports.R
+++ R/imports.R
@@ -1,11 +1,20 @@
 #' @importFrom dplyr filter mutate select arrange group_by ungroup %>%
 #' @import rlang
-#' @importFrom tibble tibble tribble
+#' @importFrom tibble tibble tribble as_tibble
 #' @importFrom utils head tail
 #' @importFrom measurements conv_unit
 NULL
 
 #' @export
 dplyr::`%>%`
+
+#' @export
+tibble::tibble
+
+#' @export
+tibble::tribble
+
+#' @export
+tibble::as_tibble
 
 globalVariables(c(".data"))
