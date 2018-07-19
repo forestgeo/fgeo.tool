b6dfbf67c7fea5b4e7129e2c6901e8f038995bbc
maurolepore <maurolepore@gmail.com>
2018-07-19 07:23:59 -0400

    Rename to pick_dbh_largest().

--- tests/testthat/test-pick_dbh_largest.R
+++ tests/testthat/test-pick_dbh_largest.R
@@ -1,4 +1,4 @@
-context("by_treeid_pick_dbh_max")
+context("pick_dbh_largest")
 
 library(dplyr)
 
@@ -11,22 +11,22 @@ cns <- tibble::tribble(
     NA, "sp2",     "2",   "2.3"
 )
 
-describe("by_treeid_pick_dbh_max()", {
+describe("pick_dbh_largest()", {
   it("outputs the same groups as input", {
     # Ungrouped
-    collapsed <- by_treeid_pick_dbh_max(cns)
+    collapsed <- pick_dbh_largest(cns)
     expect_equal(group_vars(collapsed), group_vars(cns))
     
     # Grouped
     bysp <- group_by(cns, sp)
-    collapsed <- by_treeid_pick_dbh_max(bysp)
+    collapsed <- pick_dbh_largest(bysp)
     expect_equal(group_vars(collapsed), group_vars(bysp))
   })
   
   it("automatically groups by CensusID", {
     cns$CensusID <- c(1, 2, 1, 2, 2)
     .cns <- arrange(cns, CensusID, treeID, stemID, dbh)
-    out <- by_treeid_pick_dbh_max(.cns)
+    out <- pick_dbh_largest(.cns)
     out <- arrange(out, CensusID, treeID, stemID, dbh)
     # Output all available censuses
     expect_equal(out$CensusID, as.double(c(1, 1, 2, 2)))
@@ -41,7 +41,7 @@ describe("by_treeid_pick_dbh_max()", {
       .cns, 
       list(dbh = 200, sp = "sp2", treeID = "2", stemID = "2.4", CensusID = 2)
     )
-    out2 <- by_treeid_pick_dbh_max(.cns2)
+    out2 <- pick_dbh_largest(.cns2)
     picked <- filter(out2, CensusID == 2, treeID == 2)
     expect_equal(picked$stemID, "2.4")
     expect_equal(picked$dbh, 200)
@@ -50,12 +50,12 @@ describe("by_treeid_pick_dbh_max()", {
   it("drops missing values of censusid if there are multiple unique censusid", {
     # Doesn't drop missing censusid if they are unambiguous (only one censusid)
     cns$CensusID <- c(1, 1, 1, 1, NA)
-    expect_silent(out <- by_treeid_pick_dbh_max(cns))
+    expect_silent(out <- pick_dbh_largest(cns))
     
     # Drops missing censusid if they are unambiguous (multiple censusid)
     cns$CensusID <- c(1, 1, 2, 2, NA)
     expect_warning(
-      out <- by_treeid_pick_dbh_max(cns),
+      out <- pick_dbh_largest(cns),
       "Dropping.*rows with missing.*values"
     )
     
@@ -65,14 +65,11 @@ describe("by_treeid_pick_dbh_max()", {
   it("rejects data with multiple values of `plotname`", {
     cns$PlotName <- c(1, 1, 2, 2, NA)
     expect_error(
-      out <- by_treeid_pick_dbh_max(cns),
+      out <- pick_dbh_largest(cns),
       "must have a single plotname"
     )
   })
-})
-
-
-describe("by_treeid_pick_dbh_*()", {
+  
   it("picks only one row per treeid per census despite ties", {
     cns <- tibble::tribble(
       ~dbh,   ~sp, ~treeID, ~stemID,
@@ -84,14 +81,9 @@ describe("by_treeid_pick_dbh_*()", {
         NA, "sp2",     "2",   "2.4"
     )
     
-    out <- by_treeid_pick_dbh_max(cns)
+    out <- pick_dbh_largest(cns)
     expect_equal(filter(out, treeID == 1)$dbh, 100)
     expect_equal(filter(out, treeID == 2)$dbh, 99)
-    expect_named(out, c("dbh", "sp", "treeID", "stemID"))
-    
-    out <- by_treeid_pick_dbh_min(cns)
-    expect_equal(filter(out, treeID == 1)$dbh, 10)
-    expect_equal(filter(out, treeID == 2)$dbh, 22)
     expect_named(out, c("dbh", "sp", "treeID", "stemID"))
   })
 })
