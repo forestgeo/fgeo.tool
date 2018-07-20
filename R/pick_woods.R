0fa789dee3776ba1f639399b8b5de0eb0ca6be1b
maurolepore <maurolepore@gmail.com>
2018-07-19 20:47:36 -0400

    Add pick_woods().

---
+++ R/pick_woods.R
@@ -0,0 +1,61 @@
+# fgeo.tool ---------------------------------------------------------------
+
+#' Factory of functions to pick woods and then apply a function.
+#'
+#' @param .f A function to apply after picking woods. For example, `.f =
+#'   identity` returns the picked woods unchanged.
+#'
+#' @return
+#' @keywords internal
+#' @export
+#'
+#' @examples
+#' 
+#' @noRd
+pick_woods_f <- function(.f, .collapse = fgeo.tool::pick_dbh_largest) {
+  force(.f)
+  
+  function(.data, ...) {
+    stopifnot(is.data.frame(.data))
+    # Lowercase names and groups for work with both census and ViewFullTable
+    .x <- set_names(.data, tolower)
+    .x <- groups_lower(.x)
+    
+    if (multiple_plotname(.x)) {
+      stop("`.x` must have a single plotname.", call. = FALSE)
+    }
+    
+    if (multiple_censusid(.x)) {
+      .x <- fgeo.base::drop_if_na(.x, "censusid")
+      .x <- dplyr::group_by(.x, .data$censusid, add = TRUE)
+    }
+    
+    # do() prefferred to by_group() to not drop empty groups (they result in 0L)
+    dots <- rlang::enquos(...)
+    out <- dplyr::do(
+      .x, picked_woods_f_impl(., !!! dots, .collapse = .collapse, .f = .f)
+    )
+    
+    # Restore original names; then original groups
+    out <- rename_matches(out, .data)
+    groups_restore(out, .data)
+  }
+}
+
+#' Pick woods after collapsing multiple stems by treeid and censusid.
+#' 
+#' 
+#' 
+#' @export
+#' @rdname pick_woods
+pick_woods <- pick_woods_f(identity, .collapse = fgeo.tool::pick_dbh_largest)
+
+multiple_plotname <- fgeo.base::multiple_var("plotname")
+
+multiple_censusid <- fgeo.base::multiple_var("censusid")
+
+picked_woods_f_impl <- function(.data, ..., .collapse, .f) {
+  .dots <- rlang::enquos(...)
+  pick <- dplyr::filter( .collapse(.data), !!! .dots)
+  .f(pick)
+}
