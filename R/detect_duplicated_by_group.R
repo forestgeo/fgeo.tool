ac1fda63aa8f1b0e8fac58bc42bec955b999a45c
maurolepore <maurolepore@gmail.com>
2018-08-24 08:03:15 -0400

    Add flag multiple by groups.

--- R/detect_duplicated_by_group.R
+++ R/detect_duplicated_by_group.R
@@ -8,40 +8,27 @@ detect_duplicated_by_group_f <- function(name) {
   }
 }
 
-# TODO: Add flag_multiple_by_group_f then remove duplication
-flag_predicate_by_group_f <- function(name, cond = rlang::warn, predicate, prefix) {
+flag_predicate_by_group_f <- function(name, cond, predicate, prefix) {
   force(name)
   function(.data, msg = NULL) {
     stopifnot(length(cond) == 1)
 
     nested <- tidyr::nest(.data)$data
     detected <- any(purrr::map_lgl(nested, predicate(name)))
-    customized <- c(paste0(prefix, " values were detected.\n"), msg)
-    if (detected) cond(msg %||% customized)
+    if (detected) cond(msg %||% glue("{prefix} values were detected."))
 
     invisible(.data)
   }
 }
 
-
-flag_duplicated_by_group_f <- function(name, cond = rlang::warn) {
-  flag_predicate_by_group_f(name, cond, fgeo.base::detect_duplicated_f, "Duplicated")
+flag_duplicated_by_group_f <- function(name, cond = warn) {
+  flag_predicate_by_group_f(
+    name, cond, fgeo.base::detect_duplicated_f, "Duplicated"
+  )
 }
 
-
-
-# # TODO: Add flag_multiple_by_group_f then remove duplication
-# flag_duplicated_by_group_f <- function(name, cond = rlang::warn) {
-#   force(name)
-#   function(.data, msg = NULL) {
-#     stopifnot(length(cond) == 1)
-# 
-#     nested <- tidyr::nest(.data)$data
-#     detected <- any(purrr::map_lgl(nested, fgeo.base::detect_duplicated_f(name)))
-#     customized <- c("Duplicated values were detected.\n", msg)
-#     if (detected) cond(msg %||% customized)
-# 
-#     invisible(.data)
-#   }
-# }
-
+flag_multiple_by_group_f <- function(name, cond = warn) {
+  flag_predicate_by_group_f(
+    name, cond, fgeo.base::detect_multiple_f, "Multiple"
+  )
+}
--- tests/testthat/test-detect_duplicated_by_group.R
+++ tests/testthat/test-detect_duplicated_by_group.R
@@ -1,38 +1,6 @@
 library(tibble)
 library(rlang)
 
-# FIXME: Rename file
-
-context("flag_duplicated_by_group_f")
-
-describe("flag_duplicated_by_group_f", {
-  tree <- tibble::tibble(CensusID = c(1, 2), treeID = c(1, 1))
-  msg <- "Duplicated values were detected"
-  
-  it("handles grouped data", {
-    by_censusid <- group_by(tree, CensusID)
-    expect_silent(flag_duplicated_by_group_f("treeID")(by_censusid))
-    expect_warning(flag_duplicated_by_group_f("treeID")(tree), msg)
-  })
-
-  it("handles multiple conditions and a custom message", {
-    expect_message(flag_duplicated_by_group_f("treeID", message)(tree), msg)
-    expect_error(flag_duplicated_by_group_f("treeID", rlang::abort)(tree), msg)
-    
-    custom_msg <- "Custom message"
-    flag_dup_treeid_by_grp <- flag_duplicated_by_group_f("treeID", rlang::warn)
-    expect_warning(flag_dup_treeid_by_grp(tree, custom_msg), custom_msg)
-  })
-
-  it("is insensitive to case of variable names", {
-    vft <- tibble(TreeID = c(1, 2))
-    expect_false(detect_duplicated_by_group_f("treeid")(vft))
-    expect_silent(flag_duplicated_by_group_f("treeid", rlang::warn)(vft))
-  })
-})
-
-
-
 context("detect_duplicated_by_group_f")
 
 describe("detect_duplicated_by_group_f", {
@@ -59,3 +27,63 @@ describe("detect_duplicated_by_group_f", {
   })
 })
 
+
+
+context("flag_duplicated_by_group_f")
+
+describe("flag_duplicated_by_group_f", {
+  tree <- tibble::tibble(CensusID = c(1, 2), treeID = c(1, 1))
+  msg <- "Duplicated values were detected"
+  
+  it("handles grouped data", {
+    by_censusid <- group_by(tree, CensusID)
+    expect_silent(flag_duplicated_by_group_f("treeID")(by_censusid))
+    expect_warning(flag_duplicated_by_group_f("treeID")(tree), msg)
+  })
+  
+  it("handles multiple conditions and a custom message", {
+    expect_message(flag_duplicated_by_group_f("treeID", message)(tree), msg)
+    expect_error(flag_duplicated_by_group_f("treeID", rlang::abort)(tree), msg)
+    
+    custom_msg <- "Custom message"
+    flag_dup_treeid_by_grp <- flag_duplicated_by_group_f("treeID", rlang::warn)
+    expect_warning(flag_dup_treeid_by_grp(tree, custom_msg), custom_msg)
+  })
+  
+  it("is insensitive to case of variable names", {
+    vft <- tibble(TreeID = c(1, 2))
+    expect_false(detect_duplicated_by_group_f("treeid")(vft))
+    expect_silent(flag_duplicated_by_group_f("treeid", rlang::warn)(vft))
+  })
+})
+
+
+
+context("flag_multiple_by_group_f")
+
+describe("flag_multiple_by_group_f", {
+  tree <- tibble::tibble(CensusID = c(1, 2), treeID = c(1, 2))
+  msg <- "Multiple values were detected"
+  
+  it("handles grouped data", {
+    by_censusid <- group_by(tree, CensusID)
+    expect_silent(flag_multiple_by_group_f("treeID")(by_censusid))
+    expect_warning(flag_multiple_by_group_f("treeID")(tree), msg)
+  })
+  
+  it("handles multiple conditions and a custom message", {
+    expect_message(flag_multiple_by_group_f("treeID", message)(tree), msg)
+    expect_error(flag_multiple_by_group_f("treeID", rlang::abort)(tree), msg)
+    
+    custom_msg <- "Custom message"
+    flag_dup_treeid_by_grp <- flag_multiple_by_group_f("treeID", rlang::warn)
+    expect_warning(flag_dup_treeid_by_grp(tree, custom_msg), custom_msg)
+  })
+  
+  it("is insensitive to case of variable names", {
+    vft <- tibble(TreeID = c(1, 1))
+    expect_silent(flag_multiple_by_group_f("treeid", rlang::warn)(vft))
+    vft <- tibble(TreeID = c(1, 2))
+    expect_warning(flag_multiple_by_group_f("treeid", rlang::warn)(vft))
+  })
+})
