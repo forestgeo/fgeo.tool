0fd29ce886f58fc2f0c4de77a1cb6bdd0c565555
maurolepore <maurolepore@gmail.com>
2018-08-24 07:51:47 -0400

    Extract duplication in predicate.

--- R/detect_duplicated_by_group.R
+++ R/detect_duplicated_by_group.R
@@ -9,17 +9,39 @@ detect_duplicated_by_group_f <- function(name) {
 }
 
 # TODO: Add flag_multiple_by_group_f then remove duplication
-flag_duplicated_by_group_f <- function(name, cond = rlang::warn) {
+flag_predicate_by_group_f <- function(name, cond = rlang::warn, predicate, prefix) {
   force(name)
   function(.data, msg = NULL) {
     stopifnot(length(cond) == 1)
 
     nested <- tidyr::nest(.data)$data
-    detected <- any(purrr::map_lgl(nested, fgeo.base::detect_duplicated_f(name)))
-    customized <- c("Duplicated values were detected.\n", msg)
+    detected <- any(purrr::map_lgl(nested, predicate(name)))
+    customized <- c(paste0(prefix, " values were detected.\n"), msg)
     if (detected) cond(msg %||% customized)
 
     invisible(.data)
   }
 }
+
+
+flag_duplicated_by_group_f <- function(name, cond = rlang::warn) {
+  flag_predicate_by_group_f(name, cond, fgeo.base::detect_duplicated_f, "Duplicated")
+}
+
+
+
+# # TODO: Add flag_multiple_by_group_f then remove duplication
+# flag_duplicated_by_group_f <- function(name, cond = rlang::warn) {
+#   force(name)
+#   function(.data, msg = NULL) {
+#     stopifnot(length(cond) == 1)
+# 
+#     nested <- tidyr::nest(.data)$data
+#     detected <- any(purrr::map_lgl(nested, fgeo.base::detect_duplicated_f(name)))
+#     customized <- c("Duplicated values were detected.\n", msg)
+#     if (detected) cond(msg %||% customized)
+# 
+#     invisible(.data)
+#   }
+# }
 
