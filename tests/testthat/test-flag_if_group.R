3fccbda0c4a3c70385f1c9617811c0281f6fe733
maurolepore <maurolepore@gmail.com>
2018-08-25 09:49:35 -0400

    Vary tests.

--- tests/testthat/test-flag_if_group.R
+++ tests/testthat/test-flag_if_group.R
@@ -5,21 +5,28 @@ library(rlang)
 library(fgeo.base)
 
 describe("flag_if_group", {
-  tree <- tibble::tibble(CensusID = c(1, 2), treeID = c(1, 2))
   msg <- "Flagged values were detected"
   
   it("handles grouped data", {
+    tree <- tibble::tibble(CensusID = c(1, 2), treeID = c(1, 2))
     by_censusid <- group_by(tree, CensusID)
     expect_silent(flag_if_group(by_censusid, "treeID", is_multiple))
     expect_warning(flag_if_group(tree, "treeID", is_multiple), msg)
+    
+    tree <- tibble::tibble(CensusID = c(1, 2), treeID = c(1, 1))
+    by_censusid <- group_by(tree, CensusID)
+    expect_silent(flag_if_group(by_censusid, "treeID", is_duplicated))
+    expect_warning(flag_if_group(tree, "treeID", is_duplicated), msg)
   })
 
   it("handles variable that is also a grouping variable", {
+    tree <- tibble::tibble(CensusID = c(1, 2), treeID = c(1, 1))
     by_censusid <- group_by(tree, CensusID)
-    expect_silent(flag_if_group(by_censusid, "CensusID", is_multiple))
+    expect_silent(flag_if_group(by_censusid, "CensusID", is_duplicated))
   })
 
   it("handles multiple conditions and a custom message", {
+    tree <- tibble::tibble(CensusID = c(1, 2), treeID = c(1, 2))
     expect_warning(flag_if_group(tree, "treeID", is_multiple, warn), msg)
     expect_message(flag_if_group(tree, "treeID", is_multiple, inform), msg)
     expect_error(flag_if_group(tree, "treeID", is_multiple, abort), msg)
@@ -29,6 +36,7 @@ describe("flag_if_group", {
   })
 
   it("includes in the message the name of the variable being tested", {
+    tree <- tibble::tibble(CensusID = c(1, 2), treeID = c(1, 2))
     expect_warning(flag_if_group(tree, "treeID", is_multiple), "treeID")
   })
 
