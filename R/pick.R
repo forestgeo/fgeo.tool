8b1c53d0cf353332a338f2520c8a2df9cd69b965
maurolepore <maurolepore@gmail.com>
2018-11-04 17:37:28 -0500

    Test bad inputs.

--- R/pick.R
+++ R/pick.R
@@ -1,4 +1,4 @@
-#' Pick a list of datframes or a nested dataframe in sync.
+#' Pick a list of datframes or a nested dataframe with a list-column.
 #' 
 #' This function allows you to pick rows from a key dataframe in a list or 
 #' list-column and pick the exact same rows in all other non-key dataframes
@@ -8,6 +8,8 @@
 #' @param key Key dataframe to pick rows from and use the same row indices to
 #'   pick rows from all other dataframes in the list (or list-column).
 #' @param ... Other arguments passed to methods.
+#' 
+#' @family functions to pick or drop rows of a ForestGEO dataframe
 #'
 #' @export
 #'
@@ -54,7 +56,52 @@ pick <- function(.data, ..., key = NULL) {
   as_censuses(out)
 }
 
-pick_all_rows <- function(.data, ...) UseMethod("pick_all_rows")
+pick_key_rows <- function(.data, ..., key) {
+  key_df <- tibble::rowid_to_column(pull_key_df(.data, key))
+  dplyr::filter(key_df, !!! enquos(...))$rowid
+}
+
+pull_key_df <- function(.data, ...) {
+  UseMethod("pull_key_df")
+}
+
+pull_key_df.default <- function(.data, ...) {
+  abort_bad_class(.data)
+}
+
+pull_key_df.censuses_lst <- function(.data, key) {
+  
+  if (!is.null(key)) {
+    stopifnot(length(key) == 1)
+    
+    valid_key <- key %in% names(.data)
+    if (valid_key) 
+      return(.data[[key]])
+      
+    abort_invalid_key(key)
+  }
+  
+  .data[[1]]
+}
+
+pull_key_df.censuses_tbl <- function(.data, key) {
+  
+  key <- key %||% .data[[1]][[1]]
+  stopifnot(length(key) == 1)
+  
+  key_group <- .data[[1]] %in% key
+  if (!any(key_group)) abort_invalid_key(key)
+  
+  .data[key_group, ]$data[[1]]
+}
+
+abort_invalid_key <- function(key) {
+  abort(glue("'{key}' not found."))
+}
+
+pick_all_rows <- function(.data, ...) {
+  UseMethod("pick_all_rows")
+}
 
 pick_all_rows.censuses_lst <- function(.data, .rowid) {
   purrr::map(.data, ~.x[.rowid, ])
@@ -63,26 +110,3 @@ pick_all_rows.censuses_lst <- function(.data, .rowid) {
 pick_all_rows.censuses_tbl <- function(.data, .rowid) {
   dplyr::mutate(.data, data = purrr::map(data, ~.x[.rowid, ]))
 } 
-
-
-pick_key_rows <- function(.data, ..., key) {
-  key_df <- tibble::rowid_to_column(pull_key_df(.data, key))
-  dplyr::filter(key_df, !!! enquos(...))$rowid
-}
-
-pull_key_df <- function(.data, ...) UseMethod("pull_key_df")
-
-pull_key_df.censuses_lst <- function(.data, key) {
-  if (!is.null(key)) {
-    return(.data[[key]])
-  } 
-  
-  .data[[1]]
-}
-
-pull_key_df.censuses_tbl <- function(.data, key) {
-  key <- key %||% .data[[1]][[1]]
-  key_group <- .data[[1]] %in% key
-  
-  .data[key_group, ]$data[[1]]
-}
--- tests/testthat/test-pick.R
+++ tests/testthat/test-pick.R
@@ -5,44 +5,10 @@ library(tidyr)
 library(tibble)
 library(dplyr)
 
-
-
 cns_lst <- as_censuses(list(
   c1 = tibble::tibble(dbh = 1:2),
   c2 = tibble::tibble(dbh = 8:9)
 ))
-
-test_that("with simplest inputs returns the expected data structure", {
-  expect_error(out <- pick(cns_lst, dbh == 1), NA)
-  expect_is(out, "censuses_lst")
-  expect_is(out, "list")
-  expect_named(out[[1]], "dbh")
-})
-
-test_that("picking a row in key census picks the same row in other censuses", {
-  
-  out <- pick(cns_lst, dbh == 1, key = 1)
-  expect_equal(out[[1]]$dbh, 1)
-  expect_equal(out[[2]]$dbh, 8)
-  
-  out <- pick(cns_lst, dbh == 2, key = 1)
-  expect_equal(out[[1]]$dbh, 2)
-  expect_equal(out[[2]]$dbh, 9)
-
-  out <- pick(cns_lst, dbh == 9, key = 2)
-  expect_equal(out[[1]]$dbh, 2)
-  expect_equal(out[[2]]$dbh, 9)
-  
-  out <- pick(cns_lst, dbh == 9 | dbh < 9, key = 2)
-  expect_equal(out[[1]]$dbh, 1:2)
-  expect_equal(out[[2]]$dbh, 8:9)
-  
-  out <- pick(cns_lst, dbh == 0, key = 1)
-  expect_equal(out[[1]]$dbh, integer(0))
-  expect_equal(out[[2]]$dbh, integer(0))
-})
-
-
 
 lst <- list(
   c1 = tibble::tibble(dbh = 1:2, census = 1),
@@ -54,12 +20,54 @@ ndf <- dfm %>%
   nest() %>% 
   as_censuses()
 
+
+test_that("errs with informative message", {
+  expect_error(pick(1), "Can't deal with data")
+  expect_error(pick(data.frame(x = 1)), "Can't deal with data")
+  expect_error(pick(tibble::tibble(x = 1)), "Can't deal with data")
+  expect_error(pick(list(x = 1)), "Can't deal with data")
+  
+  expect_error(pick(cns_lst, novar > 1), "'novar' not found")
+  expect_error(pick(cns_lst, dbh == 1, key = "bad"), "'bad' not found")
+  expect_error(pick(ndf, dbh == 1, key = "bad"), "'bad' not found")
+  
+  # Pick as is. Passes
+  expect_error(pick(cns_lst), NA)
+})
+
+test_that("with simplest inputs returns the expected data structure", {
+  expect_error(out <- pick(cns_lst, dbh == 1), NA)
+  expect_is(out, "censuses_lst")
+  expect_is(out, "list")
+  expect_named(out[[1]], "dbh")
+})
+
+test_that("picking a row in key census picks the same row in other censuses", {
+  out <- pick(cns_lst, dbh == 1, key = "c1")
+  expect_equal(out[[1]]$dbh, 1)
+  expect_equal(out[[2]]$dbh, 8)
+  
+  out <- pick(cns_lst, dbh == 2, key = "c1")
+  expect_equal(out[[1]]$dbh, 2)
+  expect_equal(out[[2]]$dbh, 9)
+
+  out <- pick(cns_lst, dbh == 9, key = "c2")
+  expect_equal(out[[1]]$dbh, 2)
+  expect_equal(out[[2]]$dbh, 9)
+  
+  out <- pick(cns_lst, dbh == 9 | dbh < 9, key = "c2")
+  expect_equal(out[[1]]$dbh, 1:2)
+  expect_equal(out[[2]]$dbh, 8:9)
+  
+  out <- pick(cns_lst, dbh == 0, key = "c1")
+  expect_equal(out[[1]]$dbh, integer(0))
+  expect_equal(out[[2]]$dbh, integer(0))
+})
+
 test_that("with the simplest call returns the expected data structure", {
   out <- pick(ndf, dbh == 1)
   expect_is(out, "censuses_tbl")
-  
 })
-
 
 test_that("works with nested dataframe and numeric nesting-group", {
   
