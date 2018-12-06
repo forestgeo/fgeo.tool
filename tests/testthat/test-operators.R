c0b48ccf6f2d2ce160a498c23bb70c49e46a1aec
maurolepore <maurolepore@gmail.com>
2018-12-06 10:16:32 -0500

    Export %||% directly from rlang.

--- tests/testthat/test-operators.R
+++
@@ -1,8 +0,0 @@
-context("operators")
-
-test_that("outputs as expected", {
-  expect_identical(1 %||% 2, 1)
-  expect_identical(NULL %||% 2, 2)
-  expect_identical(NULL %||% NA, NA)
-  expect_identical(NA %||% 2, NA)
-})
