8628dae7ff3828709860c5d50537294958c7867d
maurolepore <maurolepore@gmail.com>
2018-12-11 14:27:52 -0500

    read_censuses() -> read_censuses_df().

--- R/censuses.R
+++ R/censuses.R
@@ -1,32 +1,5 @@
 # TODO: Test
 
-#' Read ForestGEO censuses.
-#' 
-#' This function reads any number of ForestGEO censuses stored in .rdata files
-#' and stores them in a nested dataframe of class 'censuses_df', which has a
-#' convenient print method and enables using specific methods for working with
-#' ForestGEO data.
-#' 
-#' @seealso [rdata_df()], [as_censuses()].
-#'
-#' @inheritParams rdata_df
-#' 
-#' @family functions to import ForestGEO data
-#'
-#' @return An object of class 'censuses_df'.
-#' @export
-#'
-#' @examples
-#' censuses <- read_censuses(tool_example("rdata"))
-#' class(censuses)
-#' censuses$data
-#' 
-#' pick(censuses, dbh > 50)$data
-read_censuses <- function(path_dir, .match = NULL, .id = "censuses") {
-  dfm <- rdata_df(path_dir = path_dir, .match = .match, .id = .id)
-  as_censuses(tidyr::nest(dfm, -.id))
-}
-
 #' Create objects of class `censuses_*`, where `*` depends on the input.
 #'
 #' @param .data A ForestGEO-like dataset.
--- tests/testthat/test-censuses.R
+++ tests/testthat/test-censuses.R
@@ -1,5 +1,1 @@
-context("censuses")
 
-test_that("outputs an object of class 'censuses_df'", {
-  expect_is(read_censuses(tool_example("rdata")), "censuses_df")
-})
