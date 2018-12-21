cd50702a450b5d33b9ca2e8ac652232f57e4f5aa
maurolepore <maurolepore@gmail.com>
2018-12-20 19:38:55 -0500

    Improve tool_example() based on readr::readr_example().

--- R/tool_example.R
+++ R/tool_example.R
@@ -11,6 +11,11 @@
 #' tool_example("csv")
 #' dir(tool_example("csv"))
 #' csv_list(tool_example("csv"))
-tool_example <- function(path) {
-  system.file("extdata", path, package = "fgeo.tool")
+tool_example <- function (path = NULL) {
+  if (is.null(path)) {
+    dir(system.file("extdata", package = "fgeo.tool"))
+  }
+  else {
+    system.file("extdata", path, package = "fgeo.tool", mustWork = TRUE)
+  }
 }
