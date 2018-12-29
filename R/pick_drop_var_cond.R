e94c1d0dac8f35b5afc7e2d525c9de3c7f8c8dd3
Mauro Lepore <maurolepore@gmail.com>
2018-12-29 09:41:24 -0500

    Improve pick* docs (#122)
    * Improve examples of pick().
    * Document that pick_*() also work with ViewFullTables.
    * Refactor

--- R/pick_drop_var_cond.R
+++ R/pick_drop_var_cond.R
@@ -1,14 +1,14 @@
 #' Pick and drop rows of a ForestGEO ViewFullTable or census table.
 #'
-#' These functions provide an expressive and convenient way to filter
-#' ForestGEO's data. They let you remove missing values (with `na.rm = TRUE`)
-#' but conservatively default to keeping NAs. This behavior is similar to base
-#' subsetting and unlike `dplyr::filter()`; this conservative default matters
-#' since the difference between missing trees and dead trees is important --
-#' and you may want to include missing trees in your analysis.
+#' These functions provide an expressive and convenient way to pick specific
+#' rows from ForestGEO datasets. They let you remove missing values (with 
+#' `na.rm = TRUE`) but conservatively default to keeping NAs. This behavior is
+#' similar to base subsetting and unlike `dplyr::filter()`; this conservative
+#' default matters since the difference between missing trees and dead trees is
+#' important -- and you may want to include missing trees in your analysis.
 #'
-#' @param dfm Dataframe; A ForestGEO stem- or tree-table.
-#' @param x An atomic vector; a single value against to compare each value of
+#' @param .data A ForestGEO-like dataframe (stem, tree, or ViewFullTable).
+#' @param value An atomic vector; a single value against to compare each value of
 #'   the variable encoded in the function name.
 #' @param na.rm Set to `TRUE` if you want to remove missing values from the
 #'   variable encoded in the function name.
@@ -18,37 +18,69 @@
 #' @return Dataframe rows with matching conditions.
 #'
 #' @examples
-#' cns <- data.frame(
-#'   dbh = c(0, 50, 100, 150, NA, NA, NA),
-#'   status = c(rep("A", 4), "M", "D", NA)
-#'   # stringsAsFactors = FALSE
-#' )
-#' cns
-#'
+#' census <- tribble(
+#'   ~dbh, ~status,
+#'      0,     "A",
+#'     50,     "A",
+#'    100,     "A",
+#'    150,     "A",
+#'     NA,     "M",
+#'     NA,     "D",
+#'     NA,      NA
+#'   )
+#' 
 #' # <=
-#' pick_dbh_max(cns, 100)
-#' pick_dbh_max(cns, 100, na.rm = TRUE)
+#' pick_dbh_max(census, 100)
+#' pick_dbh_max(census, 100, na.rm = TRUE)
+#' 
 #' # >=
-#' pick_dbh_min(cns, 100)
-#' pick_dbh_min(cns, 100, na.rm = TRUE)
+#' pick_dbh_min(census, 100)
+#' pick_dbh_min(census, 100, na.rm = TRUE)
+#' 
 #' # <
-#' pick_dbh_under(cns, 100)
-#' pick_dbh_under(cns, 100, na.rm = TRUE)
+#' pick_dbh_under(census, 100)
+#' pick_dbh_under(census, 100, na.rm = TRUE)
+#' 
 #' # >
-#' pick_dbh_over(cns, 100)
-#' pick_dbh_over(cns, 100, na.rm = TRUE)
+#' pick_dbh_over(census, 100)
+#' pick_dbh_over(census, 100, na.rm = TRUE)
 #' # Same, but `subset()` does not let you keep NAs.
-#' subset(cns, dbh > 100)
-#'
+#' subset(census, dbh > 100)
+#' 
 #' # ==
-#' pick_status(cns, "A")
-#' pick_status(cns, "A", na.rm = TRUE)
+#' pick_status(census, "A")
+#' pick_status(census, "A", na.rm = TRUE)
+#' 
 #' # !=
-#' drop_status(cns, "D")
-#' drop_status(cns, "D", na.rm = TRUE)
-#'
+#' drop_status(census, "D")
+#' drop_status(census, "D", na.rm = TRUE)
+#' 
 #' # Compose
-#' pick_dbh_over(drop_status(cns, "D", na.rm = TRUE), 100)
+#' pick_dbh_over(
+#'   drop_status(census, "D", na.rm = TRUE), 
+#'   100
+#' )
+#' 
+#' # More readable as a pipiline
+#' census %>%
+#'   drop_status("D", na.rm = TRUE) %>%
+#'   pick_dbh_over(100)
+#'  
+#' # Also works with ViewFullTables
+#' vft <- tribble(
+#'   ~DBH,   ~Status,
+#'      0,   "alive",
+#'     50,   "alive",
+#'    100,   "alive",
+#'    150,   "alive",
+#'     NA, "missing",
+#'     NA,    "dead",
+#'     NA,        NA
+#' )
+#' 
+#' pick_dbh_max(vft, 100)
+#' 
+#' pick_status(vft, "alive",  na.rm = TRUE)
 #' 
 #' @family functions for fgeo census and vft
 #' @family functions for fgeo census
@@ -57,27 +89,31 @@
 #' @name pick_drop
 NULL
 
-var_cond_x <- function(var, cond) {
-  force(var)
+var_cond_x <- function(variable, cond) {
+  force(variable)
   force(cond)
-  function(dfm, x, na.rm = FALSE) {
-    stopifnot(is.data.frame(dfm))
-    stopifnot(!missing(x), is.logical(na.rm), length(x) == 1)
+  function(.data, value, na.rm = FALSE) {
+    stopifnot(is.data.frame(.data))
+    stopifnot(!missing(value), is.logical(na.rm), length(value) == 1)
 
-    old <- names(dfm)
-    names(dfm) <- tolower(names(dfm))
-
-    .var <- dfm[[tolower(var)]]
-    rows <- do.call(cond, list(.var, x))
-    if (na.rm) {
-      dfm <- dfm[rows & !is.na(rows), , drop = FALSE]
-    } else {
-      dfm <- dfm[rows | is.na(rows), , drop = FALSE]
+    pick_rows <- function(.data_, value, na.rm) {
+      rows <- do.call(cond, list(.data_[[tolower(variable)]], value))
+      if (na.rm) {
+        exclude_na(.data_, rows) 
+      } else {
+        include_na(.data_, rows)
+      }
     }
 
-    names(dfm) <- old
-    dfm
+    result <- pick_rows(set_names(.data, tolower), value, na.rm)
+    set_names(result, names(.data))
   }
+}
+exclude_na <- function(.data, rows) {
+  .data[rows & !is.na(rows), , drop = FALSE]
+}
+include_na <- function(.data, rows) {
+  .data[rows |  is.na(rows), , drop = FALSE]
 }
 
 #' @rdname pick_drop
