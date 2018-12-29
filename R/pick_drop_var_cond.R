8e58020cc8c296521cb359bc1f514c29f4ba15db
maurolepore <maurolepore@gmail.com>
2018-12-29 08:38:14 -0500

    Document that pick_*() also work with ViewFullTables.

--- R/pick_drop_var_cond.R
+++ R/pick_drop_var_cond.R
@@ -1,13 +1,13 @@
 #' Pick and drop rows of a ForestGEO ViewFullTable or census table.
 #'
-#' These functions provide an expressive and convenient way to filter
-#' ForestGEO's data. They let you remove missing values (with `na.rm = TRUE`)
-#' but conservatively default to keeping NAs. This behavior is similar to base
-#' subsetting and unlike `dplyr::filter()`; this conservative default matters
-#' since the difference between missing trees and dead trees is important --
-#' and you may want to include missing trees in your analysis.
+#' These functions provide an expressive and convenient way to pick specific
+#' rows from ForestGEO datasets. They let you remove missing values (with 
+#' `na.rm = TRUE`) but conservatively default to keeping NAs. This behavior is
+#' similar to base subsetting and unlike `dplyr::filter()`; this conservative
+#' default matters since the difference between missing trees and dead trees is
+#' important -- and you may want to include missing trees in your analysis.
 #'
-#' @param dfm Dataframe; A ForestGEO stem- or tree-table.
+#' @param dfm A ForestGEO-like dataframe (stem, tree, or ViewFullTable).
 #' @param x An atomic vector; a single value against to compare each value of
 #'   the variable encoded in the function name.
 #' @param na.rm Set to `TRUE` if you want to remove missing values from the
@@ -18,37 +18,69 @@
 #' @return Dataframe rows with matching conditions.
 #'
 #' @examples
-#' cns <- data.frame(
-#'   dbh = c(0, 50, 100, 150, NA, NA, NA),
-#'   status = c(rep("A", 4), "M", "D", NA)
-#'   # stringsAsFactors = FALSE
-#' )
-#' cns
-#'
+#' census <- tribble(
+#'   ~dbh, ~status,
+#'      0,     "A",
+#'     50,     "A",
+#'    100,     "A",
+#'    150,     "A",
+#'     NA,     "M",
+#'     NA,     "D",
+#'     NA,      NA
+#'   )
+#' 
 #' # <=
-#' pick_dbh_max(cns, 100)
-#' pick_dbh_max(cns, 100, na.rm = TRUE)
+#' pick_dbh_max(census, 100)
+#' pick_dbh_max(census, 100, na.rm = TRUE)
+#' 
 #' # >=
-#' pick_dbh_min(cns, 100)
-#' pick_dbh_min(cns, 100, na.rm = TRUE)
+#' pick_dbh_min(census, 100)
+#' pick_dbh_min(census, 100, na.rm = TRUE)
+#' 
 #' # <
-#' pick_dbh_under(cns, 100)
-#' pick_dbh_under(cns, 100, na.rm = TRUE)
+#' pick_dbh_under(census, 100)
+#' pick_dbh_under(census, 100, na.rm = TRUE)
+#' 
 #' # >
-#' pick_dbh_over(cns, 100)
-#' pick_dbh_over(cns, 100, na.rm = TRUE)
+#' pick_dbh_over(census, 100)
+#' pick_dbh_over(census, 100, na.rm = TRUE)
 #' # Same, but `subset()` does not let you keep NAs.
-#' subset(cns, dbh > 100)
-#'
+#' subset(census, dbh > 100)
+#' 
 #' # ==
-#' pick_status(cns, "A")
-#' pick_status(cns, "A", na.rm = TRUE)
+#' pick_status(census, "A")
+#' pick_status(census, "A", na.rm = TRUE)
+#' 
 #' # !=
-#' drop_status(cns, "D")
-#' drop_status(cns, "D", na.rm = TRUE)
-#'
+#' drop_status(census, "D")
+#' drop_status(census, "D", na.rm = TRUE)
+#' 
 #' # Compose
-#' pick_dbh_over(drop_status(cns, "D", na.rm = TRUE), 100)
+#' pick_dbh_over(
+#'   drop_status(census, "D", na.rm = TRUE), 
+#'   100
+#' )
+#' 
+#' # More readable as a pipiline
+#' census %>%
+#'   drop_status("D", na.rm = TRUE) %>%
+#'   pick_dbh_over(100)
+#'  
+#' # Also works with ViewFullTables
+#' vft <- tribble(
+#'   ~DBH,   ~Status,
+#'      0,   "alive",
+#'     50,   "alive",
+#'    100,   "alive",
+#'    150,   "alive",
+#'     NA, "missing",
+#'     NA,    "dead",
+#'     NA,        NA
+#' )
+#' 
+#' pick_dbh_max(vft, 100)
+#' 
+#' pick_status(vft, "alive",  na.rm = TRUE)
 #' 
 #' @family functions for fgeo census and vft
 #' @family functions for fgeo census
