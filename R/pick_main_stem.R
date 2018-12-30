9c85884508641b003d350aef1d36c9dde0ae660f
maurolepore <maurolepore@gmail.com>
2018-12-30 13:55:35 -0500

    Polish writting.

--- R/pick_main_stem.R
+++ R/pick_main_stem.R
@@ -1,38 +1,21 @@
-pick_main_f <- function(stemid = TRUE, treeid = TRUE) {
-  function(.x) {
-    stopifnot(is.data.frame(.x))
-    
-    # Store original row order to restore it at the end
-    .x <- tibble::rowid_to_column(.x)
-    # Lowercase names and groups to work with both census and ViewFullTable
-    .data <- rlang::set_names(.x, tolower)
-    # The net effect is to ignore groups: Store them now and restore them on exit.
-    .data <- groups_lower(.data)
-    
-    stopifnot_single_plotname(.data)
-    check_crucial_names(.data, c( "treeid", "stemid", "hom", "dbh"))
-    
-    .data <- pick_stemid_treeid(.data, stemid = stemid, treeid = treeid)
-    
-    # Restore rows order
-    .data <- select(arrange(.data, .data$rowid), -.data$rowid)
-    # Restore original names
-    out <- rename_matches(.data , .x)
-    # Restore original groups
-    groups_restore(out, .x)
-  }
-}
+
+
 
 #' Pick the main stem or main stemid(s) of each tree in each census.
 #' 
-#' * `pick_main_stem()`picks the main stem of each tree in each census. It
+#' * [pick_main_stem()] picks a unique row for each `treeID` per census.
+#' * [pick_main_stemid()] picks a unique row for each `stemID` per census. It is
+#' only useful when a single stem was measure twice in the same census (usually
+#' to correct for the effect of large buttresses).
+#' 
+#' * [pick_main_stem()] picks the main stem of each tree in each census. It
 #' collapses data of multi-stem trees by picking a single stem per `treeid` per
 #' `censusid`: within this group, it picks the stem at the top of a list sorted
 #' first by descending order of `hom` and then by descending order of `dbh` --
 #' this corrects the effect of buttresses and picks the main stem. It ignores
 #' groups of grouped data and rejects data with multiple plots (to work with
 #' data with multiple `plotnames` you may use `split()` or `dplyr::nest()`).
-#' * `pick_main_stemid()` does one step less than `pick_main_stem(). It only
+#' * [pick_main_stemid()] does one step less than [pick_main_stem()]. It only
 #' picks the main stemid(s) of each tree in each census and keeps all
 #' stems per treeid. This is useful when calculating the total basal area of a
 #' tree, because you need to sum the basal area of each individual stem as well as
@@ -74,6 +57,35 @@ pick_main_f <- function(stemid = TRUE, treeid = TRUE) {
 #' pick_main_stemid(census)
 #'
 #' @family functions to pick or drop rows of a ForestGEO dataframe
+#' @name pick_main_stem
+NULL
+
+pick_main_f <- function(stemid = TRUE, treeid = TRUE) {
+  function(.x) {
+    stopifnot(is.data.frame(.x))
+    
+    # Store original row order to restore it at the end
+    .x <- tibble::rowid_to_column(.x)
+    # Lowercase names and groups to work with both census and ViewFullTable
+    .data <- rlang::set_names(.x, tolower)
+    # The net effect is to ignore groups: Store them now and restore them on exit.
+    .data <- groups_lower(.data)
+    
+    stopifnot_single_plotname(.data)
+    check_crucial_names(.data, c( "treeid", "stemid", "hom", "dbh"))
+    
+    .data <- pick_stemid_treeid(.data, stemid = stemid, treeid = treeid)
+    
+    # Restore rows order
+    .data <- select(arrange(.data, .data$rowid), -.data$rowid)
+    # Restore original names
+    out <- rename_matches(.data , .x)
+    # Restore original groups
+    groups_restore(out, .x)
+  }
+}
+
+#' @rdname pick_main_stem
 #' @export
 pick_main_stem <- pick_main_f(stemid = TRUE, treeid = TRUE)
 
