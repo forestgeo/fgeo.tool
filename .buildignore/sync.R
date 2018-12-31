53e0066d2bd1cc1b0e770ed647c5694b55d9aa3b
Mauro Lepore <maurolepore@gmail.com>
2018-12-31 14:49:34 -0500

    Remove pick() (#131)
    * Remove pick()
    The plan is to generalize it as sync(), which would be similar to map() in that it takes any function, but instead of doing the acttion to all items of the list it would do it to only the `key` one and then propagate the indices to all other elements.
    
    * Draft sync()
    This commit adds pick() which should be generalized to build sync(): A function that takes any function and applies it to the `key` element of a list, then propagates the picked indices to all other elements of the list.

---
+++ .buildignore/sync.R
@@ -0,0 +1,113 @@
+#' Pick rows from a list of dataframes (e.g. a list of ForestGEO censuses).
+#' 
+#' This function allows you to pick rows from a `key` dataframe in a list
+#' and pick the same row indices in all other non-key censuses. The conditions
+#' to pick rows are checked against the `key` dataframe only. All dataframes 
+#' must have the same structure.
+#'
+#' @param .data A list of dataframes with the same names and the same number of
+#'   rows and columns.
+#' @param key Dataframe used to check the conditions passed via `...`.
+#' @param ... Other arguments passed to methods.
+#'
+#' @examples
+#' censuses <- list(
+#'   c1 = tibble(dbh = 1:2),
+#'   c2 = tibble(dbh = 8:9)
+#' )
+#' 
+#' censuses
+#' 
+#' pick(censuses, dbh == 1, key = "c1")
+#' 
+#' # Same because `key` defaults to the first element of the list
+#' pick(censuses, dbh == 1)
+#' 
+#' # Different `key`
+#' pick(censuses, dbh == 1, key = "c2")
+#' 
+#' # With multiple conditions passed via `...`
+#' pick(censuses, dbh >= 1, dbh < 2)
+#' 
+#' # `pick()` is useful after reading multiple censuses into a list
+#' rdata_files <- fgeo.x::example_path("rdata")
+#' dir(rdata_files)
+#' 
+#' censuses <- rdata_list(rdata_files)
+#' pick(censuses, dbh >= 100)
+#' 
+#' # Same in a pipeline
+#' rdata_files %>%
+#'   rdata_list() %>%
+#'   pick(dbh >= 100)
+#'
+#' @family general functions to pick or drop rows of a dataframe
+#' @export
+pick <- function(.data, ..., key = NULL) {
+  .rowid <- pick_key_rows(.data = .data, ..., key = key)
+  pick_all_rows(.data, .rowid)
+}
+
+pick_key_rows <- function(.data, ..., key) {
+  key_df <- tibble::rowid_to_column(pull_key_df(.data, key))
+  dplyr::filter(key_df, !!! enquos(...))$rowid
+}
+
+pull_key_df <- function(.data, ...) {
+  UseMethod("pull_key_df")
+}
+
+pull_key_df.default <- function(.data, ...) {
+  abort_bad_class(.data)
+}
+
+pull_key_df.list <- function(.data, key) {
+  
+  if (!is.null(key)) {
+    stopifnot(length(key) == 1)
+    
+    valid_key <- key %in% names(.data)
+    if (valid_key) 
+      return(.data[[key]])
+      
+    abort_invalid_key(key)
+  }
+  
+  .data[[1]]
+}
+
+abort_invalid_key <- function(key) {
+  abort(glue("'{key}' not found."))
+}
+
+pick_all_rows <- function(.data, ...) {
+  UseMethod("pick_all_rows")
+}
+
+pick_all_rows.list <- function(.data, .rowid) {
+  abort_bad_dim(.data)  
+  abort_bad_names(.data)  
+  purrr::map(.data, ~.x[.rowid, ])
+}
+
+abort_bad_dim <- function(.data) {
+  if (!same_dim(.data, 1) || !same_dim(.data, 2)) {
+    abort("All dataframes must have the same number of rows and columns.")
+  }
+}
+
+same_dim <- function(x, .dim) {
+  result <- unique(purrr::map_int(x, ~dim(.x)[[.dim]]))
+  identical(length(result), 1L)
+}
+
+abort_bad_names <- function(.data) {
+  if (!same_names(.data)) {
+    abort("All dataframes must have the same names.")
+  }
+}
+
+same_names <- function(x) {
+  result <- purrr::reduce(purrr::map(x, names), setdiff)
+  identical(result, character(0))
+}
