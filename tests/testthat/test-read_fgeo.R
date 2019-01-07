bedb62e39a9d19745c5b61459cb02d0c49211cb8
maurolepore <maurolepore@gmail.com>
2019-01-07 18:06:01 -0500

    Skip test if missing packages.

--- tests/testthat/test-read_fgeo.R
+++ tests/testthat/test-read_fgeo.R
@@ -38,6 +38,7 @@ test_that("read_vft guesses tab or comma separated file", {
 context("read_taxa")
 
 test_that("read_taxa can read an online file", {
+  skip_if_not_installed("pringr")
   skip_if(!pingr::is_online(), "Not online.")
 
   expect_silent(
